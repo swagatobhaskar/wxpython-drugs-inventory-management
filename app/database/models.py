5e3811f26a9fa8599357b90201d865f9ad936fe3
bhaskarswagato <bhaskarswagato@gmail.com>
2023-10-28 20:15:45 +0530

    stripped rows above 80000 in drugs-inventory-products_medicines-data.csv

--- app/database/models.py
+++ app/database/models.py
@@ -2,7 +2,7 @@ import pandas as pd
 import sqlite3
 from sqlalchemy import (
     Column, Integer, String, Float, ForeignKey, Table, create_engine, MetaData)
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import sessionmaker, Mapped, mapped_column, relationship
 from sqlalchemy.ext.declarative import declarative_base
   
 # Load CSV data into Pandas DataFrame 
@@ -21,18 +21,38 @@ customers_df.to_sql(con=engine, name='customers', if_exists='replace', index=Fal
 users_df.to_sql(con=engine, name='users', if_exists='replace', index=False)
 suppliers_df.to_sql(con=engine, name='suppliers', if_exists='replace', index=False)
 
-Base = declarative_base()
+class Base(declarative_base):
+    pass
+
+# This declaration is for overriding reflected tables
 metadata_obj = MetaData()
 
-"""Follow the declarative mapping process"""
-# class Sales(Base):
-#     __table__ = 'Sales',
-#               Column('Id', Integer(), primary_key=True),
-#               Column('customer_id', Integer(), ForeignKey),
-#               Column('product', String(255), nullable=False),
-#               Column('quantity', Integer(), nullable=False, default=1),
-#               Column('Price', Float(), nullable=False)
-# )
+"""
+id: Mapped[int] = mapped_column(primary_key=True)
+...     name: Mapped[str] = mapped_column(String(30))
+...     fullname: Mapped[Optional[str]]
+...
+...     addresses: Mapped[List["Address"]] = relationship(
+...         back_populates="user", cascade="all, delete-orphan"
+...     )
+...
+...     def __repr__(self) -> str:
+...         return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+
+"""
+class Sales(Base):
+    __tablename__ = "sales"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    customer_id: Mapped[int] = relationship(
+          back_populates="customer", cascade="all, delete-orphan"
+    )
+              Column('product', String(255), nullable=False)
+              Column('quantity', Integer(), nullable=False, default=1)
+              Column('Price', Float(), nullable=False)
+    
+    def __repr__(self) -> str:
+        return f"Customer(id={self.id!r}"
 
 Orders = Table('Orders', metadata_obj,
               Column('Id', Integer(), primary_key=True),
@@ -42,12 +62,32 @@ Orders = Table('Orders', metadata_obj,
               Column('Price', Float(), nullable=False)
 )
 
-products = Table('Products', metadata_obj, autoload=True, autoload_with=engine)
-customers = Table('Customers', metadata_obj, autoload=True, autoload_with=engine)
-mfrs = Table('Manufacturers', metadata_obj, autoload=True, autoload_with=engine)
-suppliers = Table('Suppliers', metadata_obj, autoload=True, autoload_with=engine)
-Users = Table('Users', metadata_obj, autoload=True, autoload_with=engine)
+"""Overriding the following reflected tables"""
+products = Table('Products', metadata_obj,
+                 Column('id', Integer, primary_key=True),
+                 Column('mfr_id',Integer, ForeignKey(mfrs.id))
+                 autoload_with=engine
+                 )
+customers = Table('Customers', metadata_obj,
+                  Column('id', Integer, primary_key=True),
+                  autoload_with=engine
+                  )
+mfrs = Table('Manufacturers', metadata_obj,
+             Column('id', Integer, primary_key=True),
+             autoload_with=engine
+             )
+suppliers = Table('Suppliers', metadata_obj,
+                  Column('id', Integer, primary_key=True),
+                  autoload_with=engine
+                  )
+Users = Table('Users', metadata_obj,
+              Column('id', Integer, primary_key=True),
+              autoload_with=engine
+              )
 
-metadata_obj.create_all(engine) 
+# metadata_obj.create_all(engine)
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine) 
+session = Session() 
 
-conn = engine.connect() 
+# conn = engine.connect() 
